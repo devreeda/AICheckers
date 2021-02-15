fc3f612ba0a013d2919e6083941ce6d4375c18b7
Ghouti Terki Rida <rida.ght@gmail.com>
2021-02-15 12:18:24 +0100

    modification de score

--- src/fr/istic/ia/tp1/MonteCarloTreeSearch.java
+++ src/fr/istic/ia/tp1/MonteCarloTreeSearch.java
@@ -59,9 +59,7 @@ public class MonteCarloTreeSearch {
 		 * @return Estimated probability of win for the node
 		 */
 		double score() {
-			if (n == 0)
-				return Integer.MAX_VALUE;
-			return w/n;
+			return n == 0 ? 0 : w/n;
 		}
 
 		/**
